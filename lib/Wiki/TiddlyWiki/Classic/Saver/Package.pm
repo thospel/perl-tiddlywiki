9a3e243d1da8e665956f6cbbd96e3c80b371c470
Ton Hospel <git@ton.iguana.be>
2019-11-15 15:25:33 +0100

    Add updater if it is not in wiki state yet

--- lib/Wiki/TiddlyWiki/Classic/Saver/Package.pm
+++ lib/Wiki/TiddlyWiki/Classic/Saver/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "026";
-$release_time = 1572456202;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "027";
+$release_time = 1573827925;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
